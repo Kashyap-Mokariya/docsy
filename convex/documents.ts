4f1c36cc22e00c16971b68328b26e619f7f7cf64
Kashyap Mokariya <mokariyakashyap@gmail.com>
2025-07-15 20:08:51 +0530

    Added update, remove query. Added rename, remove feature. Added open in new tab feature

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,33 +1,85 @@
-import { ConvexError, v } from "convex/values";
+import {ConvexError, v} from "convex/values";
 import {mutation, query} from "./_generated/server";
-import { paginationOptsValidator } from "convex/server";
+import {paginationOptsValidator} from "convex/server";
 
 export const createDocument = mutation({
 	args: {
 		title: v.optional(v.string()),
-		initialContent: v.optional(v.string())
+		initialContent: v.optional(v.string()),
 	},
 	handler: async (ctx, args) => {
-		const user = await ctx.auth.getUserIdentity()
+		const user = await ctx.auth.getUserIdentity();
 
-		if(!user)
-		{
-			throw new ConvexError("Unauthorized")
+		if (!user) {
+			throw new ConvexError("Unauthorized");
 		}
 
 		return await ctx.db.insert("documents", {
 			title: args.title ?? "Untitled document",
 			ownerId: user.subject,
-			initialContent: args.initialContent
-		})
+			initialContent: args.initialContent,
+		});
 	},
-})
+});
 
 export const getDocuments = query({
 	args: {
-		paginationOpts: paginationOptsValidator
+		paginationOpts: paginationOptsValidator,
 	},
 	handler: async (ctx, args) => {
 		return await ctx.db.query("documents").paginate(args.paginationOpts);
+	},
+});
+
+export const removeDocumentById = mutation({
+	args: {id: v.id("documents")},
+	handler: async (ctx, args) => {
+		const user = await ctx.auth.getUserIdentity();
+
+		if (!user) {
+			throw new ConvexError("Unauthorized");
+		}
+
+		const document = await ctx.db.get(args.id);
+
+		if (!document) {
+			throw new ConvexError("Document not found");
+		}
+
+		const isDocumentOwner = document.ownerId === user.subject;
+
+		if (!isDocumentOwner) {
+			throw new ConvexError("Unauthorized");
+		}
+
+		return await ctx.db.delete(args.id);
+	},
+});
+
+export const updateDocumentById = mutation({
+	args: {
+		id: v.id("documents"),
+		title: v.string(),
+	},
+	handler: async (ctx, args) => {
+		const user = await ctx.auth.getUserIdentity();
+
+		if (!user) {
+			throw new ConvexError("Unauthorized");
+		}
+
+		const document = await ctx.db.get(args.id);
+
+		if (!document) {
+			throw new ConvexError("Document not found");
+		}
+
+		const isDocumentOwner = document.ownerId === user.subject;
+
+		if (!isDocumentOwner) {
+			throw new ConvexError("Unauthorized");
+		}
+
+		return await ctx.db.patch(args.id, {title: args.title});
 	},
 });
--- src/app/(home)/DocumentRow.tsx
+++ src/app/(home)/DocumentRow.tsx
@@ -3,16 +3,21 @@ import { Doc } from '../../../convex/_generated/dataModel'
 import { TableCell, TableRow } from '@/components/ui/table'
 import { SiGoogledocs } from "react-icons/si"
 import { format } from "date-fns"
-import { Building2Icon, CircleUserIcon, MoreVertical } from 'lucide-react'
-import { Button } from '@/components/ui/button'
+import { Building2Icon, CircleUserIcon } from 'lucide-react'
+import DocumentMenu from './document-menu'
+import { useRouter } from 'next/navigation'
 
 interface DocumentRowProps {
     document: Doc<"documents">
 }
 
 export const DocumentRow = ({ document }: DocumentRowProps) => {
+
+    const router = useRouter()
+
     return (
         <TableRow
+            onClick={() => router.push(`/documents/${document._id}`)}
             className='cursor-pointer'
         >
             <TableCell className='w-[50px]'>
@@ -33,13 +38,11 @@ export const DocumentRow = ({ document }: DocumentRowProps) => {
                 {format(new Date(document._creationTime), "MMM dd, yyyy")}
             </TableCell>
             <TableCell className='flex justify-end'>
-                <Button
-                    variant="ghost"
-                    size="icon"
-                    className='rounded-full'
-                >
-                    <MoreVertical  className='size-4'/>
-                </Button>
+                <DocumentMenu
+                    documentId={document._id}
+                    title={document.title}
+                    onNewTab={() => window.open(`/documents/${document._id}`, "_blank")}
+                />
             </TableCell>
         </TableRow>
     )
